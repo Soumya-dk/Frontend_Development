a22f96326f951da2265b85f51deba0648f70a518
Soumya D K <soumyadk10@gmail.com>
2020-01-03 15:21:24 +0000

    Android_Soumya_Project5_toggle_relative_completed.v0.1

---
+++ AndroidStudioProjects/Project5_togglebutton_relative/app/src/main/java/com/example/project5_togglebutton_relative/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.project5_togglebutton_relative;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
